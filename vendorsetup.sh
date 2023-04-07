ffbd08f2f9034a5596416865925a0a0a578fe9dd
Kartik728 <titanupdates728@gmail.com>
2023-04-07 21:55:52 +0530

    RMX1851: fixup vendorsetup.sh
    Signed-off-by: Kartik728 <titanupdates728@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 
-banner(){
-clear
-echo "================================"
-echo "|                              |"
-echo "|   Realme 3 Pro Setup Script  |"
-echo "|       Branch: Lineage        |"
-echo "|    Maintained By: Cykeek     |"
-echo "|                              |"
-echo "================================"
-}
-
-banner 2>1
-
 # Kernel
-banner 2>1
 echo "Cloning Kernel..."
-git clone https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/proton-clang
-git clone https://github.com/Cykeek-Labs/kernel_realme_sdm710 kernel/realme/sdm710
+git clone https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/proton-clang
+git clone https://github.com/Kartik728/kernel_realme_sdm710.git kernel/realme/sdm710
 
 
 # RealmeParts
-banner 2>1
 echo "Cloning RealmeParts..."
-git clone https://github.com/Cykeek-Labs/packages_apps_realmeparts packages/apps/RealmeParts
+git clone https://github.com/Kartik728/packages_apps_realmeparts.git packages/apps/RealmeParts
