f47ff85d3c8dd5dc59cceeb30474712b8e0d4a23
Kartik728 <titanupdates728@gmail.com>
2023-05-19 18:10:54 +0530

    RMX1851: rearrange things
    Signed-off-by: Kartik728 <titanupdates728@gmail.com>

--- spark_RMX1851.mk
+++ spark_RMX1851.mk
@@ -4,16 +4,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from RMX1851 device
 $(call inherit-product, device/realme/RMX1851/device.mk)
 
-# Inherit some common SpaarkOS stuff.
+# Inherit some common SparkOS stuff.
 $(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := spark_RMX1851
 PRODUCT_DEVICE := RMX1851
 PRODUCT_BRAND := Realme
-PRODUCT_MODEL := realme 3 Pro
+PRODUCT_MODEL := Realme 3 Pro
 PRODUCT_MANUFACTURER := Realme
 
 # disable/enable blur support, default is false
@@ -28,10 +29,11 @@ TARGET_SUPPORTS_QUICK_TAP := true
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
-BUILD_FINGERPRINT := Realme/RMX1851/RMX1851:11/RKQ1.201217.002/1632356164254:user/release-keys
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="RMX1851-user 11 RKQ1.201217.002 1632356164254 release-keys" \
     PRODUCT_NAME="RMX1851"
 
-PRODUCT_GMS_CLIENTID_BASE := android-oppo
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Realme/RMX1851/RMX1851:11/RKQ1.201217.002/1632356164254:user/release-keys
